af664c604a53f3e49bcd55122f16fadb0ba70f98
scrambldchannel <sutcliffe.alex@gmail.com>
2019-07-27 23:47:43 +0200

    config tweaks

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Alexander'
 SITENAME = 'Scrambld Notations'
-SITEURL = 'https://scrambldchannel.github.io/'
+SITEURL = 'https://scrambldchannel.github.io'
 
 PATH = 'content'
 
@@ -20,20 +20,14 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
-#         ('Jinja2', 'http://jinja.pocoo.org/'),
-#         ('You can modify those links in your config file', '#'),)
-
+LINKS = (('Pelican', 'http://getpelican.com/'))
 # Social widget
-#SOCIAL = (('You can add links in your config file', '#'),
-#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Theming
 THEME = 'themes/nice-blog'
-THEME_COLOR = 'olive'
+THEME_COLOR = 'cyan'
 
 SIDEBAR_DISPLAY = ['about', 'tags']
 SIDEBAR_ABOUT = "Random musings on things like TM1, Python, data and stuff"
