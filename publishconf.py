d694afb59e669d0664ee4f3f22c2ed9859ba3cd7
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-04 16:30:14 +0200

    fix images

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,6 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://scrambldchannel.github.io'
-SITELOGO = SITEURL + "/images/scrambldchannel.jpg"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
